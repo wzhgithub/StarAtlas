acf8635bd86d36d6bd29e08e1239b4566ec2cca4
franky <87398359@qq.com>
2022-07-02 17:30:43 +0800

    fix mac m1 problem

--- star_atlas_server/build.sh
+++ star_atlas_server/build.sh
@@ -2,4 +2,4 @@
 rm -f star_atlas_server
 # go mod tidy
 # CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build .
-docker build -t star_atlas_server .
+docker build --platform=linux/amd64 -t star_atlas_server .
