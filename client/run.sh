fdd07584871e859f318349de615345800ece12fd
frankyxu1900 <107992425+frankyxu1900@users.noreply.github.com>
2022-07-03 16:13:18 +0800

    Client dev (#2)
    * init client develop
    
    * add README.MD
    
    * updated
    
    * client develop done
    
    * add build in docker
    
    * fix qme problemn
    
    * fix mac m1 problem
    
    * bug fixed
    
    * fix task count

--- client/run.sh
+++ client/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -e USER="$(id -u)" -u="$(id -u)"  -v `pwd`:/app -w /app buildessential/debian:latest bash build.sh
+if [[ ! -d "bin" ]]; then
+  mkdir -p bin
+fi
+
+docker run -e USER="$(id -u)" -u="$(id -u)"  -v `pwd`:/app -w /app --platform linux/amd64 buildessential/debian:latest bash build.sh
