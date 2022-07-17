60fb3005b7e8ff7772e453f9c9531fabfd10d8fe
franky <87398359@qq.com>
2022-07-17 15:06:34 +0800

    sperate client to stand alone container

--- client/run_tpinit.sh
+++ client/run_tpinit.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+if [[ ! -f "bin/utest" ]]; then
+  bash run.sh
+fi
+
 docker run -e USER="$(id -u)" -u="$(id -u)"  -v `pwd`:/app -w /app --platform linux/amd64 --network host buildessential/debian:latest bash tpinit.sh
