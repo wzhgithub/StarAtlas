ffc804b09241e23da28c04ea330a3d66e6e6b3e7
raydenzhang <rayz6510831@gmail.com>
2022-07-22 00:52:09 +0800

    fix comments

--- star_atlas_server/handler/failure_over_info_handler.go
+++ star_atlas_server/handler/failure_over_info_handler.go
@@ -62,10 +62,10 @@ func GetFailureOverVMCData(c *gin.Context) {
 		return
 	}
 
-	// TODO: if to_vmcdata tasks are not empty
-	// 		 set req.TransStatus = 200
+	// if to_vmcdata tasks are not empty
 	if len(to_vmcdata.APPInfo) > 0 && len(to_vmcdata.APPInfo[0].TaskSet) > 0 {
 		glog.Info("failure over request from %d to %d finished!\n", from_vmc_id, to_vmc_id)
+		// TODO: set req.TransStatus = 200
 	}
 	failure_over_vmc_data := FailureOverVMCData{}
 	failure_over_vmc_data.From = *from_vmcdata
