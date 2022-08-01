666c71f8b753ca9aa01de767f03dcc9e45495978
wangzihao <wangzihao001@gmail.com>
2022-08-01 11:12:54 +0800

    - u

--- star_atlas_server/handler/satellite_tcp_handler.go
+++ star_atlas_server/handler/satellite_tcp_handler.go
@@ -34,6 +34,18 @@ func SatelliteTCPHandler(tcpPort int) {
 	}
 }
 
+// ==============================todo case implementations =============================
+func handleByApiType(msg *pb.Msg) ([]byte, error) {
+	switch msg.Type {
+	case pb.MsgType_ApiOrbitNormal:
+	case pb.MsgType_ApiShowPicture:
+	case pb.MsgType_ApiMarkerCoordinates:
+	case pb.MsgType_ApiOrbitCoordinate:
+	}
+
+	return make([]byte, 0), nil
+}
+
 func handler(conn net.Conn, data []byte) error {
 	data = data[4:]
 	m := &pb.Msg{}
@@ -42,7 +54,12 @@ func handler(conn net.Conn, data []byte) error {
 		return err
 	}
 	//todo handler msg then send result
-	n, err := conn.Write(nil)
+	res, err := handleByApiType(m)
+	if err != nil {
+		glog.Errorf("Failed to handler message: %v\n", err)
+		return err
+	}
+	n, err := conn.Write(res)
 	if err != nil {
 		glog.Errorf("Failed to write connection message: %v\n", err)
 		return err
