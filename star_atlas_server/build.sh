48747ef35386c538cd556d209baedcaadfa4e04e
zh.wang <zh.wang@nio.com>
2023-07-17 14:32:42 +0800

    update code

--- star_atlas_server/build.sh
+++ star_atlas_server/build.sh
@@ -7,4 +7,5 @@ sh run_trans.sh
 cp -a bin/utest bin/conf bin/trans tpinit.sh ../star_atlas_server
 cd -
 chmod a+x utest && chmod a+x trans && mv tpinit.sh gen_udp.sh
+cp go.mod.bk go.mod
 docker build --platform=linux/amd64 -t star_atlas_server .
