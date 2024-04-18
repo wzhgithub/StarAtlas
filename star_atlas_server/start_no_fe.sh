05f22e05fd5b450e092bc3440d05b8d7977e8176
root <root@localhost.localdomain>
2024-04-18 17:58:16 +0800

    update code

---
+++ star_atlas_server/start_no_fe.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+docker rm -f $(docker ps -a -q)
+docker volume rm star_atlas_server_data
+bash build.sh
+docker compose -f docker-compose-no-fe.yaml up -d
--- udp_data/send_udp.py
+++ udp_data/send_udp.py
@@ -7,7 +7,7 @@ UDP_PORT = 9191
 with open(sys.argv[1], 'rb') as f:
     MESSAGE = f.read();
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    for i in range(0, 16):
+    for i in range(0, 1):
         r = sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
         print(f"send len:{r}bytes")
         time.sleep(1)
